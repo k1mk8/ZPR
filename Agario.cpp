8b3aa5f229767feee10262ad329debe6fa66f5f3
kkasper1 <karol.kasperek.stud@pw.edu.pl>
2022-12-08 04:37:10 +0100

    główny plik

--- Agario.cpp
+++ Agario.cpp
@@ -1,18 +1,43 @@
 #include "Game.h"
-
-
+#include "Interface.h"
+#include "GameOver.h"
 
 int main()
 {
     srand(static_cast<unsigned>(time(0)));
-
-    Game game;
-
-    while(game.running())
+    int choice = 0;
+    while(true)
     {
-        game.update();
-        game.render();
+        Interface interface;
+        bool change = false;
+        while(interface.running() && change == false)
+        {
+            change = interface.update();
+            interface.render();
+        }
+        interface.~Interface();
+        Game game;
+        cout<<game.running()<<endl;
+        while(game.running())
+        {
+            game.update();
+            game.render();
+        }
+        game.~Game();
+        GameOver gameOver;
+        while(choice == 0)
+        {
+            choice = gameOver.buttonPressed();
+            gameOver.render();
+        }
+        if(choice == 1){
+            choice = 0;
+        }
+        else if(choice == 2){
+            gameOver.~GameOver();
+            return 0;
+        }
+        gameOver.~GameOver();
     }
-
     return 0;
 }
